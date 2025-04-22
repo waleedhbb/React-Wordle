6f1b0db2636a16f3337879540419b2b89dda3f0c
waleedhbb <waleedalhbbab@gmail.com>
2025-04-22 19:48:27 +0300

    fix issues with deploying site

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,27 +1,45 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React from 'react';
 
 export default function Keypad({ usedKeys }) {
-const [letters, setLetters] = useState(null)
-
-useEffect(() => {
-    fetch('http://localhost:3001/letters')
-    .then (res => res.json())
-    .then(json => {
-        setLetters(json)
-        })
- } ,[])
+  const letters = [
+    { key: "a" },
+    { key: "b" },
+    { key: "c" },
+    { key: "d" },
+    { key: "e" },
+    { key: "f" },
+    { key: "g" },
+    { key: "h" },
+    { key: "i" },
+    { key: "j" },
+    { key: "k" },
+    { key: "l" },
+    { key: "m" },
+    { key: "n" },
+    { key: "o" },
+    { key: "p" },
+    { key: "q" },
+    { key: "r" },
+    { key: "s" },
+    { key: "t" },
+    { key: "u" },
+    { key: "v" },
+    { key: "w" },
+    { key: "x" },
+    { key: "y" },
+    { key: "z" }
+  ];
 
   return (
-    <div className='keypad'>
-        {letters && letters.map((l) => {
-            const color = usedKeys[l.key]
-            return (
-                <div key = {l.key} className={color}>{l.key}</div>
-
-            )
-        })}
-      
+    <div className="keypad">
+      {letters.map((l) => {
+        const color = usedKeys[l.key]; // Color based on usedKeys
+        return (
+          <div key={l.key} className={color}>
+            {l.key}
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
